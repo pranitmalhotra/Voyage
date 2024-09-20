440afb109007faf91fdae53a974df864ae5f70a0
Pranit Malhotra <pranit.malhotra2021@vitstudent.ac.in>
2024-09-20 13:41:09 +0530

    test: opening the cors for testing purpose

--- user-service/main.py
+++ user-service/main.py
@@ -10,14 +10,16 @@ from routes import form, utils
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500"
-]
+# origins = [
+#     "http://127.0.0.1:5500",
+#     "http://localhost:5500",
+#     "http://localhost:3000"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
